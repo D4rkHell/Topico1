d99411d2ebff9ccf321cf53e6b09fa707a0bb35f
D4rkHell <fcomirandau@gmail.com>
2019-05-26 08:36:23 -0400

    fp-groth terminado

--- codigo/fp-growthvstiempo.py
+++ codigo/fp-growthvstiempo.py
@@ -8,27 +8,17 @@ import os
 if os.path.exists("Resultados") is False:
     os.mkdir("Resultados")
 
-train2506 = open('Resultados/train2506.txt', 'a')
-train2507 = open('Resultados/train2507.txt', 'a')
-train2508 = open('Resultados/train2508.txt', 'a')
-train2509 = open('Resultados/train2509.txt', 'a')
-train2510 = open('Resultados/train2510.txt', 'a')
-train5006 = open('Resultados/train5006.txt', 'a')
-train5007 = open('Resultados/train5007.txt', 'a')
-train5008 = open('Resultados/train5008.txt', 'a')
-train5009 = open('Resultados/train5009.txt', 'a')
-train5010 = open('Resultados/train5010.txt', 'a')
+train502 = open('Resultados/train502.txt', 'a')
+train504 = open('Resultados/train504.txt', 'a')
+train506 = open('Resultados/train506.txt', 'a')
+train508 = open('Resultados/train508.txt', 'a')
+train510 = open('Resultados/train510.txt', 'a')
 
-prior2506 = open('Resultados/prior2506.txt', 'a')
-prior2507 = open('Resultados/prior2507.txt', 'a')
-prior2508 = open('Resultados/prior2508.txt', 'a')
-prior2509 = open('Resultados/prior2509.txt', 'a')
-prior2510 = open('Resultados/prior2510.txt', 'a')
-prior5006 = open('Resultados/prior5006.txt', 'a')
-prior5007 = open('Resultados/prior5007.txt', 'a')
-prior5008 = open('Resultados/prior5008.txt', 'a')
-prior5009 = open('Resultados/prior5009.txt', 'a')
-prior5010 = open('Resultados/prior5010.txt', 'a')
+prior102 = open('Resultados/prior102.txt', 'a')
+prior104 = open('Resultados/prior104.txt', 'a')
+prior106 = open('Resultados/prior106.txt', 'a')
+prior108 = open('Resultados/prior108.txt', 'a')
+prior110 = open('Resultados/prior110.txt', 'a')
 
 def Supp_Time(_arch, _tabla, items, supp):
     star_time = datetime.datetime.now()
@@ -39,110 +29,94 @@ def Supp_Time(_arch, _tabla, items, supp):
     #print(end_time)
     total = end_time - star_time
     #print(fpass)
-    _arch.write(str(total)+str(supp)+"\n")
+    _arch.write(str(total)+"\n"+str(supp)+"\n")
     return fpass
 
 def Guardar(_arch, tabla):
     for k,v in tabla.items():
         _arch.write(str(k)+str(v)+"\n")
 
-order_products_prior = pd.read_csv('BD/order_products__prior.csv', sep=',')
+
+print("inicio " + str(datetime.datetime.now()))
 order_products_train = pd.read_csv('BD/order_products__train.csv', sep=',')
 products = pd.read_csv('BD/products.csv', sep=',')
-
+print("cargo csv\n")
 Tabla_Comp_Train = TablaCompra(products, order_products_train)
+print("genera compra\n")
 
-train25 = []
-largo_train25 = int(len(Tabla_Comp_Train)/4)
-train50 = []
-largo_train50 = int(len(Tabla_Comp_Train)/2)
+print("elimino csv train\n")
+del order_products_train
+
+train5 = []
+largo_train5 = int(len(Tabla_Comp_Train)/20)
+print(largo_train5)
 i=0
-while i < largo_train25:
+while i < largo_train5:
     rand = randrange(0,len(Tabla_Comp_Train))
-    train25.append(Tabla_Comp_Train[rand])
+    train5.append(Tabla_Comp_Train[rand])
     i = i + 1
-i=0
-while i < largo_train50:
-    rand = randrange(0, len(Tabla_Comp_Train))
-    train50.append(Tabla_Comp_Train[rand])
-    i = i + 1
+print("hizo random")
 del Tabla_Comp_Train
-datos = Supp_Time(train2506, train25, 3, 0.6)
-Guardar(datos, train2506)
-datos = None
-datos = Supp_Time(train5006, train50, 3, 0.6)
-Guardar(datos, train5006)
-datos = None
-datos = Supp_Time(train2507, train25, 3, 0.7)
-Guardar(datos, train2507)
-datos = None
-datos = Supp_Time(train5007, train50, 3, 0.7)
-Guardar(datos, train5007)
-datos = None
-datos = Supp_Time(train2508, train25, 3, 0.8)
-Guardar(datos, train2508)
-datos = None
-datos = Supp_Time(train5008, train50, 3, 0.8)
-Guardar(datos, train5008)
-datos = None
-datos = Supp_Time(train2509, train25, 3, 0.9)
-Guardar(datos, train2509)
-datos = None
-datos = Supp_Time(train5009, train50, 3, 0.9)
-Guardar(datos, train5009)
-datos = None
-datos = Supp_Time(train2510, train25, 3, 1.0)
-Guardar(datos, train2510)
-datos = None
-datos = Supp_Time(train5010, train50, 3, 1.0)
-Guardar(datos, train5010)
-datos = None
 
+
+datos = Supp_Time(train502, train5, 3, 0.2)
+Guardar(train502, datos)
+del datos
+print("fin supp 0.2\n")
+datos = Supp_Time(train504, train5, 3, 0.4)
+Guardar(train504, datos)
+del datos
+print("fin supp 0.4\n")
+datos = Supp_Time(train506, train5, 3, 0.6)
+Guardar(train506, datos)
+del datos
+print("fin supp 0.6\n")
+datos = Supp_Time(train508, train5, 3, 0.8)
+Guardar(train508, datos)
+del datos
+print("fin supp 0.8\n")
+datos = Supp_Time(train510, train5, 3, 1.0)
+Guardar(train510, datos)
+del datos
+print("fin supp 1.0\n")
+
+print("fin train " + str(datetime.datetime.now()))
+
+print("comienza prior" + str(datetime.datetime.now()))
+
+order_products_prior = pd.read_csv('BD/order_products__prior.csv', sep=',')
+products = pd.read_csv('BD/products.csv', sep=',')
+print("cargo csv\n")
 Tabla_Comp_Prior = TablaCompra(products, order_products_prior)
-#print(len(Tabla_Comp_Prior))
+del order_products_prior
 
-prior25 = []
-largo_prior25 = int(len(Tabla_Comp_Prior)/4)
-prior50 = []
-largo_prior50 = int(len(Tabla_Comp_Prior)/2)
+prior1 = []
+largo_prior1 = int(len(Tabla_Comp_Prior)/100)
 i=0
-while i < largo_prior25:
-    rand = randrange(0,len(Tabla_Comp_Train))
-    prior25.append(Tabla_Comp_Prior[rand])
+while i < largo_prior1:
+    rand = randrange(0,len(Tabla_Comp_Prior))
+    prior1.append(Tabla_Comp_Prior[rand])
     i = i + 1
-i=0
-while i < largo_prior50:
-    rand = randrange(0, len(Tabla_Comp_Train))
-    prior50.append(Tabla_Comp_Prior[rand])
-    i = i + 1
+print("se genero random prior\n")
 del Tabla_Comp_Prior
-datos = Supp_Time(prior2506, prior25, 3, 0.6)
-Guardar(datos, prior2506)
-datos = None
-datos = Supp_Time(prior5006, prior50, 3, 0.6)
-Guardar(datos, prior5006)
-datos = None
-datos = Supp_Time(prior2507, prior25, 3, 0.7)
-Guardar(datos, train2507)
-datos = None
-datos = Supp_Time(prior5007, prior50, 3, 0.7)
-Guardar(datos, prior5007)
-datos = None
-datos = Supp_Time(prior2508, prior25, 3, 0.8)
-Guardar(datos, prior2508)
-datos = None
-datos = Supp_Time(prior5008, prior50, 3, 0.8)
-Guardar(datos, prior5008)
-datos = None
-datos = Supp_Time(prior2509, prior25, 3, 0.9)
-Guardar(datos, train2509)
-datos = None
-datos = Supp_Time(prior5009, prior50, 3, 0.9)
-Guardar(datos, train5009)
-datos = None
-datos = Supp_Time(prior2510, prior25, 3, 1.0)
-Guardar(datos, train2510)
-datos = None
-datos = Supp_Time(prior5010, prior50, 3, 1.0)
-Guardar(datos, prior5010)
-datos = None
+datos = Supp_Time(prior102, prior1, 3, 0.2)
+Guardar(prior102, datos)
+del datos
+print("fin supp 0.2\n")
+datos = Supp_Time(prior104, prior1, 3, 0.4)
+Guardar(prior104, datos)
+del datos
+print("fin supp 0.4\n")
+datos = Supp_Time(prior106, prior1, 3, 0.6)
+Guardar(prior106, datos)
+del datos
+print("fin supp 0.6\n")
+datos = Supp_Time(prior108, prior1, 3, 0.8)
+Guardar(prior108, datos)
+del datos
+print("fin supp 0.8\n")
+datos = Supp_Time(prior110, prior1, 3, 1.0)
+Guardar(prior110, datos)
+del datos
+print("fin supp 1.0\n")
+print("termino prior " + str(datetime.datetime.now()))
